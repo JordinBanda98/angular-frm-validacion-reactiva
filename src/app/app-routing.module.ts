c2cceb52410719dae5f0be3dbceb767f9eb1b796
Jordin Samuel Banda Damian <jbandada@everis.com>
2020-06-08 17:13:28 -0500

    validacion-reactiva

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DatosComponent } from './pages/datos/datos.component';
+import { TemplateComponent } from './pages/template/template.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'datos', component: DatosComponent },
+  {path: 'template', component: TemplateComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/datos/datos.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl, Validators, FormArray } from '@angular/forms'
+import { Observable } from 'rxjs';
+
+
+@Component({
+  selector: 'app-datos',
+  templateUrl: './datos.component.html',
+  styleUrls: ['./datos.component.scss']
+})
+export class DatosComponent implements OnInit {
+  forma : FormGroup;
+  clientes : string[] = ['Claro','Entel','BCP','BBVA','Interbank'];
+  rangos : string[] = ['administrador','usuario'];
+  constructor() { 
+    this.forma = new FormGroup({
+      nombreCompleto : new FormGroup({
+        nombre: new FormControl('', [Validators.required, Validators.minLength(4), this.noJordin]),
+        apellido: new FormControl('', [Validators.required, Validators.minLength(3)])
+      }),
+      usuario: new FormControl('', [Validators.required], this.existe),
+      pass : new FormControl('',Validators.required),
+      pass2 : new FormControl('',[Validators.required]),
+      cliente : new FormControl('',Validators.required),
+      rango : new  FormControl('',[Validators.required,this.noUsuario]),
+      vacaciones : new FormControl(false,Validators.required),
+      correo: new FormControl('', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]),
+      intereses: new FormArray([
+        new FormControl('', Validators.required)
+      ])
+    });
+
+    this.forma.controls.pass2.setValidators([
+      Validators.required,
+      this.noIgual.bind(this.forma) // this.forma = this
+    ]);
+  }
+
+  ngOnInit(): void {
+  }
+  guardar(){
+    console.log(this.forma);
+  }
+  agregarIntereses(){
+    const intereses = this.forma.controls.intereses as FormArray;
+    intereses.push(new FormControl('',Validators.required));
+  }
+
+  noJordin(control: FormControl): { [s: string]: boolean } {
+    if (control.value === 'Jordin') {
+      return { noJordin: true };
+    }
+  }
+
+  noIgual(control: FormControl): {[s: string] : boolean}{
+    const forma : any = this; // this = this.form
+    if (control.value !== forma.controls.pass.value) {
+      return {noIgual : true};
+    }
+  }
+
+  noUsuario(control : FormControl): {[s : string] : boolean}{
+    if (control.value === 'usuario') {
+      return{ noUsuario : true}
+    }
+  }
+
+  existe(control: FormControl): Promise<any> | Observable<any> {
+    const promesa = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          if (control.value === 'jordin') {
+            resolve({ existe: true });
+          } else {
+            resolve(null);
+          }
+        }, 3000);
+      }
+    );
+    return promesa;
+  } 
+
+}
